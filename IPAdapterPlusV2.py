17bcf94d57854e054e6c6ebf51d16a9091e2c24d
chflame <chflame@163.com>
2024-04-27 18:41:23 +0800

    fix bug with batched weights; add face output to FaceID node

--- IPAdapterPlusV2.py
+++ IPAdapterPlusV2.py
@@ -240,7 +240,7 @@ def ipadapter_execute(model,
                         face_cond_embeds.append(torch.from_numpy(face[0].normed_embedding).unsqueeze(0))
                     else:
                         face_cond_embeds.append(torch.from_numpy(face[0].embedding).unsqueeze(0))
-                    image.append(image_to_tensor(face_align.norm_crop(image_iface[i], landmark=face[0].kps, image_size=256)))
+                    image.append(image_to_tensor(face_align.norm_crop(image_iface[i], landmark=face[0].kps, image_size=256 if is_sdxl else 224)))
 
                     if 640 not in size:
                         print(f"\033[33mINFO: InsightFace detection resolution lowered to {size}.\033[0m")
@@ -270,7 +270,9 @@ def ipadapter_execute(model,
                 img_uncond_embeds = torch.zeros_like(img_cond_embeds)
             if image_composition is not None:
                 img_comp_cond_embeds = img_comp_cond_embeds.image_embeds
-        del image, image_negative, image_composition
+        del image_negative, image_composition
+        
+        image = None if not is_faceid else image # if it's face_id we need the cropped face for later
     elif pos_embed is not None:
         img_cond_embeds = pos_embed
 
@@ -398,8 +400,7 @@ def ipadapter_execute(model,
             set_model_patch_replace(model, patch_kwargs, ("middle", 0, index))
             patch_kwargs["number"] += 1
 
-    return model
-
+    return (model, image)
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -602,7 +603,7 @@ class IPAdapterSimpleV2:
         if 'clipvision' not in ipadapter:
             raise Exception("CLIPVision model not present in the pipeline. Please load the models with the IPAdapterUnifiedLoader node.")
 
-        return (ipadapter_execute(model.clone(), ipadapter['ipadapter']['model'], ipadapter['clipvision']['model'], **ipa_args), )
+        return ipadapter_execute(model.clone(), ipadapter['ipadapter']['model'], ipadapter['clipvision']['model'], **ipa_args)
 
 class IPAdapterAdvancedV2:
     def __init__(self):
@@ -655,13 +656,16 @@ class IPAdapterAdvancedV2:
                 image_composition = image_style
 
             weight_type = "strong style and composition" if expand_style else "style and composition"
-        elif ipadapter_params is not None: # we are doing batch processing
+        if ipadapter_params is not None: # we are doing batch processing
             image = ipadapter_params['image']
             attn_mask = ipadapter_params['attn_mask']
             weight = ipadapter_params['weight']
             weight_type = ipadapter_params['weight_type']
             start_at = ipadapter_params['start_at']
             end_at = ipadapter_params['end_at']
+        else:
+            # at this point weight can be a list from the batch-weight or a single float
+            weight = [weight]
 
         image = image if isinstance(image, list) else [image]
 
@@ -675,7 +679,7 @@ class IPAdapterAdvancedV2:
                 "image": image[i],
                 "image_composition": image_composition,
                 "image_negative": image_negative,
-                "weight": weight if not isinstance(weight, list) else weight[i],
+                "weight": weight[i],
                 "weight_composition": weight_composition,
                 "weight_faceidv2": weight_faceidv2,
                 "weight_type": weight_type if not isinstance(weight_type, list) else weight_type[i],
@@ -689,10 +693,10 @@ class IPAdapterAdvancedV2:
                 "layer_weights": layer_weights,
             }
 
-            work_model = ipadapter_execute(work_model, ipadapter_model, clip_vision, **ipa_args)
+            work_model, face_image = ipadapter_execute(work_model, ipadapter_model, clip_vision, **ipa_args)
 
         del ipadapter
-        return (work_model, )
+        return (work_model, face_image, )
 
 class IPAdapterBatchV2(IPAdapterAdvancedV2):
     def __init__(self):
@@ -795,6 +799,8 @@ class IPAdapterFaceIDV2(IPAdapterAdvancedV2):
         }
 
     CATEGORY = "ipadapter/faceid"
+    RETURN_TYPES = ("MODEL","IMAGE",)
+    RETURN_NAMES = ("MODEL", "face_image", )
 
 class IPAAdapterFaceIDBatchV2(IPAdapterFaceIDV2):
     def __init__(self):
@@ -931,7 +937,7 @@ class IPAdapterTiledV2:
                 "embeds_scaling": embeds_scaling,
             }
             # apply the ipadapter to the model without cloning it
-            model = ipadapter_execute(model, ipadapter_model, clip_vision, **ipa_args)
+            model, _ = ipadapter_execute(model, ipadapter_model, clip_vision, **ipa_args)
 
         return (model, torch.cat(tiles), torch.cat(masks), )
 
@@ -1009,7 +1015,7 @@ class IPAdapterEmbedsV2:
 
         del ipadapter
 
-        return (ipadapter_execute(model.clone(), ipadapter_model, clip_vision, **ipa_args), )
+        return ipadapter_execute(model.clone(), ipadapter_model, clip_vision, **ipa_args)
 
 class IPAdapterMSV2(IPAdapterAdvancedV2):
     @classmethod
